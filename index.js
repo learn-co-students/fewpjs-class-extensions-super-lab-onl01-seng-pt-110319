bfdb484adb5bce12a690f72b587b754825356f5d
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 11:20:18 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ class Tree {
 }
 class Deciduous extends Tree{
   constructor(species, name){
-    
-    
+    super(species);
+    this.name = name;
   }
+  
+  static definition(){
+    return (super.definition + 'Deciduous trees shed their leaves annually.')
+  }
+  
+}
+
+class Evergreen extends Tree{
+  
 }
